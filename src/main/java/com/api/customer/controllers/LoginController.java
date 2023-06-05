3ae07e1947955228c5d8c0665d66f0a017d959be
thutrang <thupagevo@gmail.com>
2023-06-05 11:42:29 +0700

    loginwithgoogle

--- src/main/java/com/api/customer/controllers/LoginController.java
+++ src/main/java/com/api/customer/controllers/LoginController.java
@@ -1,32 +1,29 @@
 package com.api.customer.controllers;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.ClientAuthorizationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.api.customer.utils.GoogleUtils;
-
-import jakarta.servlet.http.HttpServletRequest;
-
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/management")
 public class LoginController {
 
-    @Autowired
-    private GoogleUtils googleUtils;
-
-    @RequestMapping("/login-google")
-    public String loginGoogle(HttpServletRequest request) throws ClientAuthorizationException, IOException {
-        String code = request.getParameter("code");
-
-        if (code == null || code.isEmpty()) {
-            return "redirect:/login?google=error";
-        }
-
-        googleUtils.getToken(code);
-        return "redirect:/user";
+    @GetMapping(value = "/login")
+    public String gettoken(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient auth2AuthorizedClient,
+            @AuthenticationPrincipal OAuth2User user) {
+        String name = user.getAttribute("name");
+        String email = user.getAttribute("email");
+        model.addAttribute("name", name);
+        model.addAttribute("email", email);
+        OAuth2AccessToken accessToken = auth2AuthorizedClient.getAccessToken();
+        String token = accessToken.getTokenValue();
+        model.addAttribute("token", token);
+        return "index";
     }
 }
